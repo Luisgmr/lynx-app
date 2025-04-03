8d9f64080f647e10af9a3cf14f7128e47629a94f
Luis Gustavo Miranda <luisgmr.privado@hotmail.com>
2025-04-03 17:03:14 -0300

    feat: lynx typography

--- app/src/main/java/com/luisgmr/lynxapp/presentation/theme/Type.kt
+++ app/src/main/java/com/luisgmr/lynxapp/presentation/theme/Type.kt
@@ -1,34 +1,36 @@
 package com.luisgmr.lynxapp.presentation.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.luisgmr.lynxapp.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+fun fontFamily() = FontFamily(
+    Font(R.font.rethink_sans_regular, weight = FontWeight.Normal),
+    Font(R.font.rethink_sans_medium, weight = FontWeight.Medium),
+    Font(R.font.rethink_sans_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.rethink_sans_bold, weight = FontWeight.Bold),
+    Font(R.font.rethink_sans_extra_bold, weight = FontWeight.ExtraBold),
 )
+
+val Typography = Typography().run {
+    copy(
+        displaySmall = displaySmall.copy(fontFamily = fontFamily()),
+        displayMedium = displayMedium.copy(fontFamily = fontFamily()),
+        displayLarge = displayLarge.copy(fontFamily = fontFamily()),
+        headlineSmall = displayLarge.copy(fontFamily = fontFamily()),
+        headlineMedium = displayLarge.copy(fontFamily = fontFamily()),
+        headlineLarge = displayLarge.copy(fontFamily = fontFamily()),
+        titleSmall = displayLarge.copy(fontFamily = fontFamily()),
+        titleMedium = displayLarge.copy(fontFamily = fontFamily()),
+        titleLarge = displayLarge.copy(fontFamily = fontFamily()),
+        bodySmall = displayLarge.copy(fontFamily = fontFamily()),
+        bodyMedium = displayLarge.copy(fontFamily = fontFamily()),
+        bodyLarge = displayLarge.copy(fontFamily = fontFamily()),
+        labelSmall = displayLarge.copy(fontFamily = fontFamily()),
+        labelMedium = displayLarge.copy(fontFamily = fontFamily()),
+        labelLarge = displayLarge.copy(fontFamily = fontFamily())
+    )
+}
+
