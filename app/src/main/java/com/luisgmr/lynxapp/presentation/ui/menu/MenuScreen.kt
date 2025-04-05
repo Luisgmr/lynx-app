27290b01bce01a53a5f31b02c345c33daba7cbb4
Luis Gustavo Miranda <luisgmr.privado@hotmail.com>
2025-04-05 00:58:28 -0300

    feat: menu screen icons

--- app/src/main/java/com/luisgmr/lynxapp/presentation/ui/menu/MenuScreen.kt
+++ app/src/main/java/com/luisgmr/lynxapp/presentation/ui/menu/MenuScreen.kt
@@ -2,6 +2,7 @@ package com.luisgmr.lynxapp.presentation.ui.menu
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,30 +14,41 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.Button
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.adamglin.PhosphorIcons
+import com.adamglin.phosphoricons.Bold
+import com.adamglin.phosphoricons.Fill
+import com.adamglin.phosphoricons.Regular
+import com.adamglin.phosphoricons.bold.Cat
+import com.adamglin.phosphoricons.fill.Cat
+import com.adamglin.phosphoricons.fill.Exam
+import com.adamglin.phosphoricons.fill.Student
+import com.adamglin.phosphoricons.regular.Cat
 import com.luisgmr.lynxapp.R
-import com.luisgmr.lynxapp.presentation.components.LynxScaffold
 import com.luisgmr.lynxapp.presentation.navigation.Screen
 import com.luisgmr.lynxapp.presentation.theme.LynxAppTheme
 
+@Preview
 @Composable
 fun MenuScreen(
-    navController: NavController
+    navController: NavController = rememberNavController()
 ) {
     LynxAppTheme {
         Column(
@@ -47,12 +59,18 @@ fun MenuScreen(
             Column(
                 modifier = Modifier.padding(24.dp)
             ) {
-                Text(
-                    text = stringResource(R.string.app_name),
-                    style = MaterialTheme.typography.headlineSmall,
-                    fontWeight = FontWeight.SemiBold,
-                    color = Color.White
-                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Icon(PhosphorIcons.Regular.Cat, contentDescription = stringResource(R.string.icon), Modifier.size(36.dp), tint = Color.White)
+                    Text(
+                        text = stringResource(R.string.app_name),
+                        style = MaterialTheme.typography.headlineSmall,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.White
+                    )
+                }
                 Spacer(Modifier.size(16.dp))
                 Text(
                     text = "Bem-vindo(a), Luis!",
@@ -64,7 +82,10 @@ fun MenuScreen(
                 modifier = Modifier
                     .weight(1f)
                     .fillMaxWidth()
-                    .background(Color(0xffeaeaea), shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp))
+                    .background(
+                        Color(0xffeaeaea),
+                        shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)
+                    )
                     .padding(24.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
@@ -81,29 +102,46 @@ fun MenuScreen(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.spacedBy(24.dp)
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .size(128.dp)
-                            .background(Color.White, shape = RoundedCornerShape(16.dp))
-                            .clickable {
-                                navController.navigate(Screen.Students.route)
-                            },
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(stringResource(R.string.students))
-                    }
-                    Column(
-                        modifier = Modifier
-                            .size(128.dp)
-                            .background(Color.White, shape = RoundedCornerShape(16.dp)),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(stringResource(R.string.subjects))
-                    }
+                    ScreenNavigationButton(
+                        stringResource(R.string.students),
+                        PhosphorIcons.Fill.Student,
+                        navController
+                    )
+                    ScreenNavigationButton(
+                        stringResource(R.string.subjects),
+                        PhosphorIcons.Fill.Exam,
+                        navController
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ScreenNavigationButton(
+    text: String,
+    icon: ImageVector,
+    navController: NavController
+) {
+    Column(
+        modifier = Modifier
+            .size(128.dp)
+            .background(Color.White, shape = RoundedCornerShape(16.dp))
+            .clip(RoundedCornerShape(16.dp))
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(
+                    bounded = true,
+                ),
+                onClick = {
+                    navController.navigate(Screen.Students.route)
+                }
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(icon, contentDescription = stringResource(R.string.icon), Modifier.size(36.dp))
+        Text(text)
     }
 }
